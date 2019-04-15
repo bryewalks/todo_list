371f2da1dcd06b61588acbcd1c745297d01bc20e
bryewalks <bryewalks@gmail.com>
2019-04-15 17:14:11 -0500

    before props'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import Checkbox from './components/Checkbox'
+
+
+function App() {
+  return (
+          <div>
+            <input type='checkbox'/><span>todo one</span><br/>
+            <input type='checkbox'/><span>todo two</span><br/>
+            <input type='checkbox'/><span>todo three</span><br/>
+          </div>
+    )
 }
 
-export default App;
+export default App
+
---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+          <h1>this is a MainContent</h1>
+    )
+}
+
+export default MainContent
